65f48522de8db30edbf208c36827959cf5c8e6f5
MrFlava <thatelitemaili33t@gmail.com>
2022-02-09 17:14:23 +0200

    Updated tests.py

--- tests.py
+++ tests.py
@@ -2,44 +2,27 @@ import os
 import unittest
 
 from requests_module import RequestsModule
-from main import make_request, parse_response
+from main import make_request, parse_response, run
 from settings_reader import read_settings_from_json
 
 
 class TestScrapingFunctions(unittest.TestCase):
 
-    def test_settings_reader(self):
-        dir_path = os.path.join(os.path.dirname(os.path.abspath(__file__)))
-        self.assertEqual(read_settings_from_json(dir_path), {"attempts": 5,
-                                                             "user_agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
-                                                                           " AppleWebKit/537.36 (KHTML, like Gecko) "
-                                                                           "Chrome/86.0.4240.80 Safari/537.36"
-                                                                           " Edg/86.0.622.48",
-                                                             "url": "https://www.boredapi.com/api/activity"})
+    def setUp(self) -> None:
+        self.dir_path = os.path.join(os.path.dirname(os.path.abspath(__file__)))
+        self.settings = read_settings_from_json(self.dir_path)
+
+        self.URL = self.settings['url']
+        self.ATTEMPTS = self.settings['attempts']
+        self.USER_AGENT = self.settings['user_agent']
+
+        self.requests_module = RequestsModule(self.ATTEMPTS, self.USER_AGENT)
 
     def test_make_request(self):
-        dir_path = os.path.join(os.path.dirname(os.path.abspath(__file__)))
-        settings = read_settings_from_json(dir_path)
-
-        URL = settings['url']
-        ATTEMPTS = settings['attempts']
-        USER_AGENT = settings['user_agent']
-
-        REQUESTS_MODULE = RequestsModule(ATTEMPTS, USER_AGENT)
-
-        self.assertEqual(make_request(URL, REQUESTS_MODULE).status_code, 200)
+        self.assertEqual(make_request(self.URL, self.requests_module).status_code, 200)
 
     def test_parse_response(self):
-        dir_path = os.path.join(os.path.dirname(os.path.abspath(__file__)))
-        settings = read_settings_from_json(dir_path)
-
-        URL = settings['url']
-        ATTEMPTS = settings['attempts']
-        USER_AGENT = settings['user_agent']
-
-        REQUESTS_MODULE = RequestsModule(ATTEMPTS, USER_AGENT)
-
-        response = make_request(URL, REQUESTS_MODULE)
+        response = make_request(self.URL, self.requests_module)
         self.assertNotEqual(parse_response(response), {})
 
 
